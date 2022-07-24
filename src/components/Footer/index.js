8caaa6ff12a25b53e97e679ccf311793770e46af
Richard Huffman <richardhuffman96@gmail.com>
2022-07-24 00:00:57 -0400

    added footer icons

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,10 +1,18 @@
 import React from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faGithubSquare, faLinkedin } from '@fortawesome/free-brands-svg-icons'
 
 function Footer() {
 
   return (
+    
     <footer>
-      &copy; 2022 Richard Huffman
+      <span>&copy; 2022 Richard Huffman</span>
+      <div className="icons">
+        <a href={'www.linkedin.com/in/richard-huffman-52806b137'} target="_blank" rel="noreferrer"><FontAwesomeIcon icon={faLinkedin} /> </a>
+        <a href={'https://github.com/chardmuffin'} target="_blank" rel="noreferrer"><FontAwesomeIcon icon={faGithubSquare} /> </a>
+      </div>
+      
     </footer>
   )
 
