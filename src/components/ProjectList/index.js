95acdc70762d7d21c4010e0b1e9b16065cd2e95c
Richard Huffman <33993562+chardmuffin@users.noreply.github.com>
2022-10-11 00:48:18 -0400

    updated project descriptions

--- src/components/ProjectList/index.js
+++ src/components/ProjectList/index.js
@@ -7,32 +7,40 @@ function ProjectList( { setCurrentTab, mobileUI } ) {
 
     const projects = [
         {
-            name: 'WIP - Bar Book',
-            tags: 'MongoDB, Express, React, Node, CSS, Apollo GraphQL',
-            description: `Bar Book is a mobile webapp that makes it easy to quickly look up cocktail recipes. React-Bootstrap components are a starting point when building the mobile-first UI. Recipe search results are fetched by API.`,
+            name: 'Portfolio',
+            tags: 'React, Node, CSS, JavaScript',
+            description: `The portofolio you are viewing is a single page app built from scratch using React.`,
+            link: 'https://chardmuffin.github.io/portfolio-v2/',
+            repo: 'https://github.com/chardmuffin/portfolio-v2',
+            photo: 'bar-book.png'
+        },
+        {
+            name: 'Bar Book',
+            tags: 'MongoDB, Express, React, Node, JavaScript, CSS, Apollo GraphQL',
+            description: `Bar Book is a solo project I made while working as a bartender to make it easier to quickly look up cocktail recipes. React-Bootstrap components were used as a starting point when building the mobile-first UI. Recipe search results are displayed via query on the Drinks DB (using Mongoose). Some sections of the app require creating an account. Once logged in (users are authorized by JWT), users are able to add custom drink recipes to the DB, view their profile, and create a Bar Book of recipes.`,
             link: 'https://bar-book.herokuapp.com/',
             repo: 'https://github.com/chardmuffin/bar-book',
             photo: 'bar-book.png'
         },
         {
             name: 'Chasing Dreams',
-            tags: 'MongoDB, Express, React, Node, CSS',
-            description: 'Chasing Dreams is a group project where the group used the MERN stack to build a travel application that helps users to find hotels and attractions at locations all over the world.',
+            tags: 'MongoDB, Express, React, Node, JavaScript, CSS',
+            description: 'Chasing Dreams was a group project. We used the MERN stack to build a travel application that helps users to find hotels and attractions at locations all over the world. Search results are fetched via API. A different API is leveraged to attempt to include a picture with each search result (my responsibility). Users can save their favorite search results.',
             link: 'https://chasing-dreams.herokuapp.com/',
             repo: 'https://github.com/Mikekuney/Chasing-Dreams',
             photo: 'chasing-dreams.png'
         },
         {
             name: 'Mental Health Buddy',
-            tags: 'HTML, CSS, Javascript',
-            description: 'Mental Health Buddy displays quotes from any of these categories: advice slips, dad jokes, zen quotes, stoicism quotes, and Kanye quotes. (A different API is leveraged for each category.) Also allows users to create daily TODO list.',
+            tags: 'HTML, CSS, JavaScript',
+            description: 'Mental Health Buddy was a group project, although I built the majority of this one. The app displays quotes from any of these categories (selected by the user): advice slips, dad jokes, zen quotes, and stoicism quotes. (A different API is leveraged for each category.) Favorite quotes can be saved and viewed via the hamburger menu. The color scheme is customizable. App also allows users to track a list of TODOs.',
             link: 'https://chardmuffin.github.io/mental-health-buddy/',
             repo: 'https://github.com/chardmuffin/mental-health-buddy',
             photo: 'mental-health-buddy.png'
         },
         {
             name: 'Code Probe',
-            tags: 'HTML, CSS, Javascript',
+            tags: 'HTML, CSS, JavaScript',
             description: 'The quiz starts, a timer counts down from 75 seconds and the user is prompted with several multiple-choice questions. (Questions and answer choices appear in a randomized order each time the quiz is taken.)',
             link: 'https://chardmuffin.github.io/code-probe/',
             repo: 'https://github.com/chardmuffin/code-probe',
@@ -40,7 +48,7 @@ function ProjectList( { setCurrentTab, mobileUI } ) {
         },
         {
             name: 'Weather Pal',
-            tags: 'HTML, CSS, Javascript',
+            tags: 'HTML, CSS, JavaScript',
             description: 'Utlizes OpenWeatherMap API to provide a summary of the current weather and a 5-day forecast for any major city.',
             link: 'https://chardmuffin.github.io/weather-pal/',
             repo: 'https://github.com/chardmuffin/weather-pal',
